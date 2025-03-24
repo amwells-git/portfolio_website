359fee18ae02adcb46b6b9b8c472fafaeaf2192a
amwells-git <amwells2021@gmail.com>
2025-03-24 13:11:38 -0500

    React Frontend v0.2 | Updated src

---
+++ frontend_react/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App'
+import './index.css';
+
+// render application to html document
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+    <App/>
+)
