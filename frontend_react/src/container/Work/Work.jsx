32ec4dd50073427641fd2ffdf3c5f5522cd61236
amwells-git <amwells2021@gmail.com>
2025-03-29 13:03:22 -0500

    React Frontend v0.65 | Removed Project Link Button Due To Lack Of Use

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -65,15 +65,16 @@ const Work = () => {
                                 whileHover={{opacity: [0,1]}}
                                 transition={{duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5}}
                                 className="app__work-hover app__flex">
-                                <a href={work.projectLink} target="_blank" rel="noreferrer">
-                                    <motion.div
-                                        whileInView={{scale: [0, 1]}}
-                                        whileHover={{scale: [1, 0.9]}}
-                                        transition={{duration: 0.25}}
-                                        className="app__flex">
-                                        <AiFillEye/>
-                                    </motion.div>
-                                </a>
+                                {/* Remove to have button to showcase link*/}
+                                {/*<a href={work.projectLink} target="_blank" rel="noreferrer">*/}
+                                {/*    <motion.div*/}
+                                {/*        whileInView={{scale: [0, 1]}}*/}
+                                {/*        whileHover={{scale: [1, 0.9]}}*/}
+                                {/*        transition={{duration: 0.25}}*/}
+                                {/*        className="app__flex">*/}
+                                {/*        <AiFillEye/>*/}
+                                {/*    </motion.div>*/}
+                                {/*</a>*/}
                                 <a href={work.codeLink} target="_blank" rel="noreferrer">
                                     <motion.div
                                         whileInView={{scale: [0, 1]}}
